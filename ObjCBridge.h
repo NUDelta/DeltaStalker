72d82d28df46a8a92c3321828f5b6da285c9e086
YK <yk@u.northwestern.edu>
2016-11-11 16:56:55 -0600

    first commit

---
+++ ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  DeltaStalker
+//
+//  Created by Yongsung on 11/11/16.
+//  Copyright © 2016 Delta. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '10.0'
+target 'DeltaStalker' do
+  pod 'EstimoteSDK'
+end
+
+
